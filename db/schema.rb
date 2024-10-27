5447121ff519e875a3db961682bb3031f281acb8
Ronan Mullaney <rmullaney@colgate.edu>
2024-10-27 19:13:21 -0400

    figuring out db stuff

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2024_10_26_234232) do
+ActiveRecord::Schema[7.2].define(version: 2024_10_27_231005) do
   create_table "books", force: :cascade do |t|
     t.string "title"
     t.string "author"
@@ -18,7 +18,7 @@ ActiveRecord::Schema[7.2].define(version: 2024_10_26_234232) do
     t.integer "pages"
     t.text "description"
     t.string "publisher"
-    t.date "publish_date"
+    t.time "publish_date"
     t.integer "isbn_13"
     t.string "language_written"
     t.datetime "created_at", null: false
