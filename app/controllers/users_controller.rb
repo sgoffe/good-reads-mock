4e7959ee9955bcf57db61599f570b568b269b434
Ronan Mullaney <rmullaney@colgate.edu>
2024-10-27 19:17:27 -0400

    User Model seems to be working just fine. Formalized testing has not been written though

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -37,7 +37,12 @@ class UsersController < ApplicationController
 
   def destroy
     @user = User.find(params[:id])
-    @user.destroy!
+    if @user.destroy
+      redirect_to users_path
+    else
+      ##error message
+    end
+
   end
 
   private
