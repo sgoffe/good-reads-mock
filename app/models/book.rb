eb9f2a0e09c39c16793c4e3b8ae99d1d862c4de3
SGoffe <sophiaagoffe@gmail.com>
2024-10-31 12:16:09 -0400

    merging book.rb, books_controller, book index view

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,4 +1,16 @@
 class Book < ApplicationRecord
   enum :genre, %i[historical_fiction fiction horror romance comedy thriller young_adult science_fiction mystery nonfiction]
-  
+
+  has_many_attached :images do |attachable|
+    attachable.variant :thumb, resize_to_limit: [ 80, 80 ]
+    attachable.variant :medium, resize_to_fill: [ 400, 400 ]
+  end
+
+  def self.pages_less_than_or_eq_to?(pagecount)
+    self.where("pages <=  ?", pagecount).order(:title)
+  end
+
+  def self.by_search_string(substr)
+    self.where("title LIKE ?", "%#{substr}%").or(self.where("author LIKE ?", "%#{substr}%")).or(self.where("description LIKE ?", "%#{substr}%"))
+  end
 end
