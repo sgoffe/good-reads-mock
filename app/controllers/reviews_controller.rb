9abe6e4f25ccc4b60dfc9e48ffda6b75dff9bafb
slwalley <shayla.walley@gmail.com>
2024-10-31 13:11:44 -0400

    Basic edit for review

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -8,16 +8,34 @@ class ReviewsController < ApplicationController
     @review = Review.find(params[:id])
   end
 
+  def new 
+
+  end
+
   def create
 
   end
-
-  def new
-
-  end
   
+  def edit
+    @review = Review.find(params[:id])
+  end
+
+  def update
+    @review = Review.find(params[:id])
+    if @review.update(create_update_params)
+      redirect_to reviews_path, notice: 'Review updated successfully'
+    else
+      flash[:alert] = 'Review could not be edited'
+      render :edit, status: :unprocessable_content
+    end
+  end
 
   def destroy
 
+  end
+
+private 
+  def create_update_params
+    params.require(:review).permit(:user, :book, :description, :rating)
   end
 end
