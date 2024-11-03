dc4dea9d29fac1449d6b58a397ade7a6a6208ad2
SheltonSS <48075441+SheltonSS@users.noreply.github.com>
2024-11-03 16:48:38 -0500

    update books schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2024_10_27_231005) do
+ActiveRecord::Schema[7.2].define(version: 2024_11_03_205800) do
   create_table "books", force: :cascade do |t|
     t.string "title"
     t.string "author"
@@ -18,8 +18,8 @@ ActiveRecord::Schema[7.2].define(version: 2024_10_27_231005) do
     t.integer "pages"
     t.text "description"
     t.string "publisher"
-    t.time "publish_date"
-    t.integer "isbn_13"
+    t.date "publish_date"
+    t.string "isbn_13"
     t.string "language_written"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
