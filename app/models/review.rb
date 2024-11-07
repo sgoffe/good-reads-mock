5b1eff4a84a9c6d939d18d27560d8834194dccd1
Emily Kociubes <ekociubes@colgate.edu>
2024-11-07 14:24:06 -0500

    added review validations

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,9 +1,13 @@
 class Review < ApplicationRecord
   belongs_to :user
   belongs_to :book
-  
+  validates :book_id, :presence => true
+  validates_associated :book
+  validates :user_id, :presence => true
+  validates_associated :user
   validates :user, presence: true
   validates :book, presence: true
   validates :rating, presence: true
   validates :description, presence: true
+
 end
