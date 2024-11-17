2f0f826fb20e307b67b797271e026170161deb57
SGoffe <sophiaagoffe@gmail.com>
2024-11-16 21:56:44 -0500

    advanced search done for now

--- app/models/book.rb
+++ app/models/book.rb
@@ -15,16 +15,30 @@ class Book < ApplicationRecord
   validates :pages, :presence => true, numericality: {greater_than_or_equal_to: 0}
   validates :description, :presence => true
   validates :publisher, :presence => true
-  #validates :publish_date, :presence => true
+  validates :publish_date, :presence => true
   validates :isbn_13, :presence => true #, format: {with: /\A\d{13}\Z/}
   validates :language_written, :presence => true
 
   def self.pages_less_than_or_eq_to?(pagecount)
-    self.where("pages <=  ?", pagecount.to_i).order(:title)
+    Book.where("pages <=  ?", pagecount.to_i).order(:title)
   end
 
   def self.by_search_string(substr)
-    self.where("title LIKE ?", "%#{substr}%").or(self.where("author LIKE ?", "%#{substr}%")).or(self.where("description LIKE ?", "%#{substr}%"))
+    words = substr.split.map { |word| "%#{word}%" }
+    Book.where(substr.split.map {|word| "(title LIKE ? OR author LIKE ? OR description LIKE ?)"}.join(" AND "), *words.flat_map { |word| [word, word, word] }) #, "%#{substr}%").or(self.where("", "%#{substr}%")).or(self.where("description LIKE ?", "%#{substr}%"))
   end
+
+  def self.with_average_rating(value)
+    Book.joins(:reviews).group("books.id").having("AVG(rating) >= ?", value)
+  end
+
+  def self.rating
+    Book.joins(:reviews).group("books.id")
+  end
+  
+  def self.in_genre(genre)
+    Book.where(genre: genre)
+  end
+ 
 
 end
