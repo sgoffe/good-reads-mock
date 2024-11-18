0d7b9356b942beb45ce1e0e4638d376357952f15
Ronan Mullaney <rmullaney@colgate.edu>
2024-11-18 11:03:38 -0500

    added delete method and updated flashed and all that still need to write tests:

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -4,14 +4,21 @@ class FriendshipsController < ApplicationController
     def create
         @friendship = Friendship.new(user_id: params[:user_id], friend_id: params[:friend_id])
         if @friendship.save
-            flash[:notice] = "Book created successfully"
+            flash[:notice] = "Friend successfully added"
             redirect_to books_path
         else
-            flash[:alert] = "Book could not be created" 
-            render :new, status: :unprocessable_entity
+            flash[:alert] = "Friend could not be added" 
+            redirect_to books_path
         end
     end
 
+
+    def destroy
+        puts params
+        @friendship = Friendship.find_by(user_id: params[:user_id], friend_id: params[:id])
+        @friendship.destroy!
+        redirect_to books_path, notice: 'Friend removed successfully'
+    end
 
 
 
--- config/routes.rb
+++ config/routes.rb
@@ -14,7 +14,7 @@ Rails.application.routes.draw do
   root "books#index"
   resources :books
   resources :users do
-    resources :friendships, only: [:new, :create]
+    resources :friendships, only: [:new, :create, :destroy]
   end
   resources :reviews
 
