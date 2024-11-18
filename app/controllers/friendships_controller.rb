95ed2aeb761b40b6e0926f46203e37b2c2774340
Ronan Mullaney <rmullaney@colgate.edu>
2024-11-18 10:47:48 -0500

    friendship creation works A user can successfully use the add a friend button and then on a revisit of the page the user is in their friends section From this point I need to add testing, and perhaps before that spruce up the page so that the redirects make semantic sense and the flash messages work and all that On a base level the friendship create method works and the index page correctly shows friends vs not friends

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -1,4 +1,18 @@
-class FriendshipsController << ApplicationController
+class FriendshipsController < ApplicationController
+
+
+    def create
+        @friendship = Friendship.new(user_id: params[:user_id], friend_id: params[:friend_id])
+        if @friendship.save
+            flash[:notice] = "Book created successfully"
+            redirect_to books_path
+        else
+            flash[:alert] = "Book could not be created" 
+            render :new, status: :unprocessable_entity
+        end
+    end
+
+
 
 
 end
