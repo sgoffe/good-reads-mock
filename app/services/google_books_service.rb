120139df14dddd3f5415615746a2d893463708c1
SheltonSS <48075441+SheltonSS@users.noreply.github.com>
2024-11-21 11:53:31 -0500

    gem stuff

---
+++ app/services/google_books_service.rb
@@ -0,0 +1,18 @@
+require 'httparty'
+
+class GoogleBooksService
+  include HTTParty
+  base_uri 'https://www.googleapis.com/books/v1'
+
+  def initialize(api_key)
+    @api_key = api_key
+  end
+
+  def search_books(query)
+    self.class.get("/volumes", query: { q: query, key: @api_key })
+  end
+
+  def get_purchase_links(volume_id)
+    self.class.get("/volumes/#{volume_id}", query: { key: @api_key })
+  end
+end
