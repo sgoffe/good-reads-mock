15c1f60460c75c06b153da8737de8f048ac39400
slwalley <shayla.walley@gmail.com>
2024-12-16 17:06:09 -0500

    Book search card styling layout done

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,24 @@ module ApplicationHelper
   def round_to_quarter(value)
     (value / 0.25).round * 0.25
   end
+
+  def render_star_rating(book)
+    if book.rating.nil?
+      content_tag(:p, "No ratings yet", class: "ms-1 text-sm font-medium text-gray-500 dark:text-gray-400")
+    else
+      rounded_rating = round_to_quarter(book.rating)
+      content_tag(:div, class: "flex items-center") do
+        (1..5).each do |i|
+          if rounded_rating >= i
+            concat content_tag(:i, '', class: "fas fa-star text-yellow-300 me-1")
+          elsif rounded_rating > (i - 1) && rounded_rating < i
+            concat content_tag(:i, '', class: "fas fa-star-half-alt text-yellow-300 me-1")
+          else
+            concat content_tag(:i, '', class: "fas fa-star text-gray-300 me-1")
+          end
+        end
+        concat content_tag(:p, "#{book.rating.round(2)} stars", class: "ms-1 text-sm font-medium text-gray-500 dark:text-gray-400")
+      end
+    end
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2024_12_14_054738) do
+ActiveRecord::Schema[8.0].define(version: 2024_12_16_220508) do
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -52,6 +52,7 @@ ActiveRecord::Schema[8.0].define(version: 2024_12_14_054738) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "img_url"
+    t.decimal "rating"
   end
 
   create_table "friendships", force: :cascade do |t|
