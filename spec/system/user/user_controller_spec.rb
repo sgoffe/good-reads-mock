fe5d14143374b12b3cd669cbd6ee73231f541409
Ronan Mullaney <rmullaney@colgate.edu>
2024-12-17 14:27:04 -0500

    cleaned up all the user and admin specs

--- spec/system/user/user_controller_spec.rb
+++ spec/system/user/user_controller_spec.rb
@@ -6,7 +6,7 @@ RSpec.describe "UserController", type: :system do
     end
 
     before(:each) do
-        @u1 = User.create!(first: "Allie", last: "Amberson", email: "aa@gmail.com", bio:"wassup", password:"aamerson", role: :admin)
+        @u1 = User.create!(first: "Allie", last: "Amberson", email: "aa@gmail.com", bio:"wassup", password:"aamberson", role: :admin)
         @u2 = User.create!(first: "Brett", last: "Boyerton", email: "bb@gmail.com", bio:"howdy", password:"bboyerton", role: :standard)
         @u3 = User.create!(first: "Charlie", last: "Chaplin", email: "cc@gmail.com", bio:"hey there", password:"cchaplin", role: :standard)
     end
@@ -41,28 +41,30 @@ RSpec.describe "UserController", type: :system do
     end
     describe "new/create methods" do 
         it "successfully creates Durk" do
-            visit users_path
-            click_on "Create new user"
-            click_on "Sign up"
+            visit books_path
+            click_on "Log In"
+            click_on "Sign up here!"
             fill_in 'First', with: "Durk"
             fill_in 'Last', with: "Deacon"
             fill_in 'Email', with: "dd@gmail.com"
             fill_in 'Password', with: "123456"
             fill_in 'Password confirmation', with: "123456"
             click_on "Sign up"
-            expect(page).to have_content("dd@gmail.com")
-            expect(page).to have_content("Log Out")
             expect(User.all.count).to eq(4)
+            expect(page).to have_selector("#profile_click")
+            find('#profile_click').click
+            expect(page).to have_current_path(profile_path)
+            expect(page).to have_content("Durk Deacon")
         end
 
         it "fails to create Durk" do
-            visit users_path
-            click_on "Create new user"
-            click_on "Sign up"
+            visit books_path
+            click_on "Log In"
+            click_on "Sign up here!"
             fill_in 'First', with: "Durk"
             click_on "Sign up"
             expect(page).to have_content("errors prohibited this user from being saved")
-            expect(page).not_to have_content("Log Out")
+            expect(page).not_to have_selector("#profile_click")
         end
     end
     describe "destroy method" do
@@ -75,33 +77,34 @@ RSpec.describe "UserController", type: :system do
             expect(page).not_to have_content("Name: ")
         end
     end
-    describe "show method" do
-        it "successfully shows Allie" do
-            visit user_path(@u2)
-            click_on "Delete"
-            visit user_path(@u3)
-            click_on "Delete"
-            click_on "More about this user"
-            expect(page).to have_content("wassup")
-            expect(page).to have_content("Back to Index")
+    describe "profile method" do
+        it "successfully shows Allie's profile" do
+            visit books_path
+            click_on "Log In"
+            fill_in 'Email', with: "aa@gmail.com"
+            fill_in 'Password', with: "aamberson"
+            click_on "Log in"
+            expect(page).to have_content("Signed in successfully.")
+            expect(page).to have_selector("#profile_click")
+            find('#profile_click').click
+            expect(page).to have_current_path(profile_path)
+            expect(page).to have_content("Bio: wassup")
         end
     end
 
-    describe "log out" do
-        it "successfully logs out a user" do
-            visit users_path
-            click_on "Create new user"
-            click_on "Sign up"
-            fill_in 'First', with: "Durk"
-            fill_in 'Last', with: "Deacon"
-            fill_in 'Email', with: "dd@gmail.com"
-            fill_in 'Password', with: "123456"
-            fill_in 'Password confirmation', with: "123456"
-            click_on "Sign up"
-            expect(page).to have_content("dd@gmail.com")
-            expect(page).to have_content("Log Out")
+    describe "log in and out" do
+        it "successfully logs in and logs out a user" do
+            visit books_path
+            click_on "Log In"
+            fill_in 'Email', with: "aa@gmail.com"
+            fill_in 'Password', with: "aamberson"
+            click_on "Log in"
+            expect(page).to have_content("Signed in successfully.")
+            expect(page).to have_selector("#profile_click")
+            find('#profile_click').click
+            expect(page).to have_current_path(profile_path)
             click_on "Log Out"
-            expect(page).to have_content("Log In")
+            expect(page).to have_content("Signed out successfully.")
         end
     end
 
