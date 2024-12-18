75f6bbb30e3d7f77fc6cedd3eb81c47dc27508c4
SheltonSS <48075441+SheltonSS@users.noreply.github.com>
2024-12-17 21:08:00 -0500

    Fixing intagration with main

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2024_12_17_013309) do
+ActiveRecord::Schema[8.0].define(version: 2024_12_17_190100) do
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
