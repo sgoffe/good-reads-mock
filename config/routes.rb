3df5afc687f5cc2a9d1beca2b38333f9b2484d30
Emily Kociubes <ekociubes@colgate.edu>
2024-12-19 16:36:27 -0500

    added some places to save books

--- config/routes.rb
+++ config/routes.rb
@@ -47,8 +47,8 @@ Rails.application.routes.draw do
   get '/profile', to: 'users#profile'
   post '/google_books/add', to: 'books#add_google_book', as: 'add_google_book'
   get '/google_books/:id', to: 'books#show_google', as: 'google_book'
-  # get '/library', to: 'lists#index', as: 'library'
   get '/users/:id/admin', to: 'users#admin', as: 'user_admin'
   get '/users/:id/admin/moderate', to: 'users#admin_moderate', as: 'user_admin_moderate'
   # get '/books/:id/reviews/:id', to: 'reviews#show'
+  post '/add_favorite/:id', to: 'lists#add_favorite', as: 'add_favorite'
 end
